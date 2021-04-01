b4541a983a4875495968944688293fca3c72b0f3
hujianglai <11111111>
2021-04-01 16:55:36 +0800

    autoconfig

---
+++ springcloud/autoconfig/testgo/src/main/java/com/yzf/hjl/WorkConfig.java
@@ -0,0 +1,15 @@
+package com.yzf.hjl;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+@Configuration
+public class WorkConfig {
+    public WorkConfig() {
+        System.out.println("ok");
+    }
+
+
+
+}
---
+++ springcloud/autoconfig/testgo/src/main/java/com/yzf/hjl/WorkGo.java
@@ -0,0 +1,11 @@
+package com.yzf.hjl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WorkGo {
+
+    public String go(){
+        return "go";
+    }
+}
